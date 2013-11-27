d279007a70b1bfd929415d5269f43404083cf111
Spencer A. Lockhart <Spence@coderslive.com>
2013-11-27 15:07:43 -0700

    Made a quick patch to pages, so that by default the url is blank instead of '/' because otherwise the default page creates is 'http://domain/dexterousURL//'

--- core/admin/pages.php
+++ core/admin/pages.php
@@ -15,7 +15,7 @@ $form = new Form('page');
 
 $form->addSection(($url[2] == 'new' ? 'New page' : 'Edit page'), '');
 $form->addText('title', 'Title', 'As displayed in the titlebar', '', array('[a-zA-Z0-9\s]*', 0, 20, 'Only alphanumeric characters and spaces allowed'));
-$form->addLinkUrl('url', 'URL', $base_url, 'title');
+$form->addLinkUrl('url', 'URL', ($base_url === '/' ? '' : $base_url), 'title');
 
 $form->setId('title', 'url-feed');
 $form->setId('url', 'url');
