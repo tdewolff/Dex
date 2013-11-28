623c10a82f3f82025b7e894edcc2f27ce1f55164
Spencer A. Lockhart <Spence@coderslive.com>
2013-11-28 06:01:56 -0700

    Hello world module - Simplified setup script to one exec statement. Simply deletes any data if it exists and recreates it on install/reinstall.

--- modules/first_module/admin/setup.php
+++ modules/first_module/admin/setup.php
@@ -1,21 +1,21 @@
 <?php
-$db->exec("
-    CREATE TABLE IF NOT EXISTS module_first_module (
+$db->exec('
+    DROP TABLE IF EXISTS module_first_module;
+
+    CREATE TABLE module_first_module (
         module_first_module_id INTEGER PRIMARY KEY,
         key TEXT,
         value TEXT
-    );");
+    );
 
-if (!$db->querySingle("SELECT * FROM link_module WHERE link_id = '0' AND module_name = 'first_module' LIMIT 1")) {
-    $db->exec("
-        INSERT INTO link_module (link_id, module_name) VALUES (
-            0,
-            'first_module'
-        );");
-}
+    DELETE FROM link_module WHERE module_name = "first_module" AND link_id = 0;
 
-$db->exec("
+    INSERT INTO link_module (link_id, module_name) VALUES (
+        0,
+        "first_module"
+    );
+
     INSERT INTO module_first_module (key, value) VALUES (
-        'message',
-        'Hello, World'
-    );");
+        "message",
+        "Hello, World"
+    );');
