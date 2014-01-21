7652db822ad2fc63a6bf87c809714a7ff2250925
Spencer A. Lockhart <Spence@coderslive.com>
2014-01-21 09:27:47 -0700

    updating themes

--- themes/simple/hooks.php
+++ themes/simple/hooks.php
@@ -4,6 +4,7 @@ Hooks::attach('site-header', -1, function () {
     Theme::set('simple');
 
     Theme::addExternalStyle('http://yui.yahooapis.com/pure/0.3.0/pure-min.css');
+    Theme::addExternalStyle('http://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic');
     Theme::addStyle('top-menu.css');
     Theme::addExternalScript('//ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js');
     Theme::addDeferredScript('script.js');
