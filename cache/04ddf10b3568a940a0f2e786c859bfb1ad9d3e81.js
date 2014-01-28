113e321be5b6737780d92090ac1161a0b8297f10
Taco de Wolff <t.de.wolff@gmail.com>
2014-01-28 10:52:01 +0100

    Cleaning up bad commit

--- cache/04ddf10b3568a940a0f2e786c859bfb1ad9d3e81.js
+++
@@ -1,7 +0,0 @@
-(function ($) {
-    console.log($('#admin-bar'));
-    if ($('#admin-bar').length !== 0) {
-        $('nav').css('top', '33px');
-        $('header').css('margin-top', '33px');
-    }
-}(jQuery));
