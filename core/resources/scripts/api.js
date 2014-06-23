969aa6898e906d141548d2ea9510b85918c4573d
Taco de Wolff <t.de.wolff@gmail.com>
2014-06-23 14:48:58 +0200

    Test case

--- core/resources/scripts/api.js
+++ core/resources/scripts/api.js
@@ -6,6 +6,9 @@ function api(url, data, success, error) {
 		}, session_time * 1000);
 	}
 
+	console.log(url);
+	console.log(data);
+
 	if (!url)
 		apiFatal('No API URL set');
 	else
@@ -15,6 +18,7 @@ function api(url, data, success, error) {
 			data: data,
 			dataType: 'json',
 			success: function (data) {
+				console.log(data);
 				if (typeof data['_error'] !== 'undefined') {
 					if (typeof error !== 'undefined' && error && error(data) === false) {
 						return;
@@ -31,6 +35,7 @@ function api(url, data, success, error) {
 				}
 			},
 			error: function (data) {
+				console.log(data);
 				if (typeof error !== 'undefined' && error && error(data) === false) {
 					return;
 				}
